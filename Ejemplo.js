2e12ae849cd2beabb25f2373ac8e1a1144b7c8c9
berenicehr22 <bereniceherrerabv26@gmail.com>
2024-10-22 13:10:59 -0500

    mi primer commit

---
+++ Ejemplo.js
@@ -0,0 +1,2 @@
+//mi primer comentario 
+ console.log("hola"+" "+"mundo")//nos muestra el resultado por consola 
---
+++ Funciones.js
@@ -0,0 +1,62 @@
+// El Area del cuadrado es:
+let l = 5
+let RES = l * l
+console.log('El area del cuadrado es:',RES) 
+
+//El Area del triangulo es:
+let b = 3
+let a = 5
+let Res = b * a
+console.log('El area del triangulo es:',Res)
+
+//El area del rectangulo es:
+let L = 3
+let A = 6
+let R = L * A
+console.log('El area del rectangulo es',R)
+
+//El Area del rombo es:
+let DM = 4
+let DN = 2
+let RS = (DM * DN)/2
+
+//El Area del circulo es:
+const pi = 3.14
+let rd = 6
+let r =  pi * rd
+console.log('El area del circulo es',r)
+
+//Funcion de Area del cuadrado
+function Cuadrado (l,l){
+    return RES = l * l
+}
+console.log('la funcion del cuadrado es:')
+console.log(Cuadrado(5,6))
+
+//Funcion de area del triangulo
+function Triangulo (b,a){
+    return RES = b * a
+}
+console.log('la funcion del triangulo es:')
+console.log(Triangulo(3,5))
+
+//Funcion de area del rectangulo
+function rectangulo (l,a){
+    return RS = l * a
+}
+console.log('la funcion del rectangulo es:')
+console.log(rectangulo(4,2))
+
+//Funcion de area de rombo
+function rombo (DM,DN){
+    return R = DM * DN/2
+}
+console.log('la funcion del rombo es:')
+console.log(rombo(4,2))
+
+//Funcion de area de circulo
+function circulo (pi,rd){
+    return r = pi * rd
+}
+console.log('la funcion del circulo es:')
+console.log(circulo(3.14,6))
